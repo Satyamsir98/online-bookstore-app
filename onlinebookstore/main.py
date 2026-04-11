6236cae6556447ab76c765badff634f51429bbeb
Satyam Rai <satyamsir98@gmail.com>
2026-04-11 21:02:59 +0530

    chnges in  bookstore backend for deploy

--- onlinebookstore/main.py
+++ onlinebookstore/main.py
@@ -34,4 +34,5 @@ def init_extensions(app):
   # This allows requests from the Angular frontend
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
+    # app.run(debug=True)
